ac95d444131093ee2a92adc2f400db3fea38c0a1
Norman Eidelman <ne58@afsconnect1.njit.edu>
2017-11-29 17:19:46 -0500

    commit

---
+++ error.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Failed</title>
+</head>
+<body>
+	<h1>No account associated with this email exists, or the password entered was incorrect</h1>
+	<a href="LoginPage.php">Return to Login Page</a>
+</body>
+</html>
