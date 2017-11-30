33150df8282faf9a4395f4bccc05a833c3b2bb75
Norman Eidelman <ne58@afsconnect1.njit.edu>
2017-11-29 19:01:21 -0500

    updated to include todos

--- welcome.php
+++ welcome.php
@@ -10,10 +10,14 @@
 
 <h1 align="middle">
 	<?php
+		$email = $_GET['email'];
 		echo "Login Successful!<br/>";
 		echo "Welcome ".$_GET['fname']." ". $_GET['lname'];
 	?>
 </h1>
+
+<?php include 'list.php';?>
+
 
 <div>
 	<h2 align="middle"><a href="LoginPage.php">Return to Log In page</a></h2>
